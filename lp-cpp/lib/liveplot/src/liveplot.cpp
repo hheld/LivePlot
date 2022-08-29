49ebb1aaecc151bef4aad55d934a282715565744
Harald Held <harald.held@gmail.com>
2022-08-29 13:41:31 +0200

    Sending messages on separate thread.

--- lp-cpp/lib/liveplot/src/liveplot.cpp
+++ lp-cpp/lib/liveplot/src/liveplot.cpp
@@ -1,6 +1,7 @@
 #include "liveplot.h"
 #include "logger.h"
 
+#include <thread>
 #include <zmq.hpp>
 
 namespace lp
@@ -11,10 +12,10 @@ namespace
 constexpr auto loggerName = "liveplot";
 }
 
-class LivePlot::d final
+class Publisher final
 {
 public:
-    d()
+    Publisher()
         : sock_(std::make_unique<zmq::socket_t>(ctx_, zmq::socket_type::push))
     {
         sock_->bind("ipc://../../../test");
@@ -28,6 +29,41 @@ public:
 private:
     zmq::context_t                 ctx_;
     std::unique_ptr<zmq::socket_t> sock_;
+};
+
+class LivePlot::d final
+{
+public:
+    d()
+        : sock_(std::make_unique<zmq::socket_t>(ctx_, zmq::socket_type::pair))
+    {
+        sock_->bind("inproc://#1");
+
+        thPub_ = std::thread([this]() {
+            zmq::socket_t sock(ctx_, zmq::socket_type::pair);
+            sock.connect("inproc://#1");
+
+            Publisher pub;
+
+            while (true)
+            {
+                zmq::message_t msg;
+                (void)sock.recv(msg);
+
+                pub.send(msg.to_string_view());
+            }
+        });
+    }
+
+    void send(std::string_view msg) const
+    {
+        sock_->send(zmq::buffer(msg), zmq::send_flags::dontwait);
+    }
+
+private:
+    zmq::context_t                 ctx_;
+    std::unique_ptr<zmq::socket_t> sock_;
+    std::thread                    thPub_;
 };
 
 LivePlot::LivePlot()
