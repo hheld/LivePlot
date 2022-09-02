ae9464fb380dc50406b49599a8156bdbd415a509
Harald Held <harald.held@gmail.com>
2022-09-02 14:06:18 +0200

    Clean up frontend.

--- live-plotter/src/App.tsx
+++ live-plotter/src/App.tsx
@@ -1,49 +1,29 @@
 import {useState} from "react";
-import reactLogo from "./assets/react.svg";
 import {invoke} from "@tauri-apps/api/tauri";
-import "./App.css";
 import {listen} from "@tauri-apps/api/event";
 
 await listen("data", (event) => {
     console.log("got event!!", event.payload);
 })
 
-function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
+const App = () => {
+    const [topic, setTopic] = useState("");
 
     const subscribe = async () => {
-        await invoke("subscribe", {quantity: name});
+        await invoke("subscribe", {quantity: topic});
     };
 
     const unsubscribe = async () => {
-        await invoke("unsubscribe", {quantity: name});
+        await invoke("unsubscribe", {quantity: topic});
     };
 
     return (
         <div className="container">
-            <h1>Welcome to Tauri!</h1>
-
-            <div className="row">
-                <a href="https://vitejs.dev" target="_blank">
-                    <img src="/vite.svg" className="logo vite" alt="Vite logo"/>
-                </a>
-                <a href="https://tauri.app" target="_blank">
-                    <img src="/tauri.svg" className="logo tauri" alt="Tauri logo"/>
-                </a>
-                <a href="https://reactjs.org" target="_blank">
-                    <img src={reactLogo} className="logo react" alt="React logo"/>
-                </a>
-            </div>
-
-            <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
             <div className="row">
                 <div>
                     <input
-                        id="greet-input"
-                        onChange={(e) => setName(e.currentTarget.value)}
-                        placeholder="Enter a name..."
+                        onChange={(e) => setTopic(e.currentTarget.value)}
+                        placeholder="Enter a topic ..."
                     />
                     <button type="button" onClick={() => subscribe()}>
                         Subscribe
@@ -53,7 +33,6 @@ function App() {
                     </button>
                 </div>
             </div>
-            <p>{greetMsg}</p>
         </div>
     );
 }
