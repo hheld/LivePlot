73d139cd91e5ad74d45fc744d257191d95416f64
Harald Held <harald.held@gmail.com>
2022-09-05 21:30:51 +0200

    Nicer way of subscribing and unsubscribing in the UI.

--- live-plotter/src/App.tsx
+++ live-plotter/src/App.tsx
@@ -1,42 +1,16 @@
-import {useState} from "react";
-import {invoke} from "@tauri-apps/api/tauri";
 import {listen} from "@tauri-apps/api/event";
 import AvailableQuantities from "./AvailableQuantities";
+import {HStack} from "@chakra-ui/react";
 
 await listen("data", (event) => {
     console.log("got event!!", event.payload);
 });
 
 const App = () => {
-    const [topic, setTopic] = useState("");
-
-    const subscribe = async () => {
-        await invoke("subscribe", {quantity: topic});
-    };
-
-    const unsubscribe = async () => {
-        await invoke("unsubscribe", {quantity: topic});
-    };
-
     return (
-        <div className="container">
-            <div className="row">
-                <div>
-                    <input
-                        onChange={(e) => setTopic(e.currentTarget.value)}
-                        placeholder="Enter a topic ..."
-                    />
-                    <button type="button" onClick={() => subscribe()}>
-                        Subscribe
-                    </button>
-                    <button type="button" onClick={() => unsubscribe()}>
-                        Unsubscribe
-                    </button>
-                </div>
-            </div>
-
+        <HStack>
             <AvailableQuantities/>
-        </div>
+        </HStack>
     );
 }
 
--- live-plotter/src/AvailableQuantities.tsx
+++ live-plotter/src/AvailableQuantities.tsx
@@ -1,10 +1,20 @@
 import {listen} from "@tauri-apps/api/event";
 import {availableQuantities, availableQuantitiesStore, storeAvailableQuantity} from "./availableQuantitiesStore";
 import {useEffect, useState} from "react";
+import {Switch, TableCaption, TableContainer, Tbody, Td, Thead, Tr, Table, Th} from "@chakra-ui/react";
+import {invoke} from "@tauri-apps/api/tauri";
 
 await listen("newQuantity", (event) => {
     storeAvailableQuantity(event.payload as string);
 });
+
+const subscribe = async (topic: string) => {
+    await invoke("subscribe", {quantity: topic});
+};
+
+const unsubscribe = async (topic: string) => {
+    await invoke("unsubscribe", {quantity: topic});
+};
 
 const AvailableQuantities = () => {
     const [quantities, setQuantities] = useState(availableQuantities());
@@ -16,25 +26,29 @@ const AvailableQuantities = () => {
     }, []);
 
     const quantitiesTable = quantities.map(q => (
-        <tr key={q}>
-            <td>{q}</td>
-        </tr>
+        <Tr key={q}>
+            <Td>{q}</Td>
+            <Td><Switch size='sm' onChange={(e) => e.currentTarget.checked ? subscribe(q) : unsubscribe(q)}/></Td>
+        </Tr>
     ));
 
     return (
-        <div>
-            <h1>Available quantities</h1>
-            <table>
-                <thead>
-                <tr>
-                    <th>Quantity</th>
-                </tr>
-                </thead>
-                <tbody>
-                {quantitiesTable}
-                </tbody>
-            </table>
-        </div>
+        <TableContainer>
+            <Table variant="striped">
+                <TableCaption>
+                    Available quantities
+                </TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>Quantity</Th>
+                        <Th>Subscribe</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
+                    {quantitiesTable}
+                </Tbody>
+            </Table>
+        </TableContainer>
     );
 }
 
