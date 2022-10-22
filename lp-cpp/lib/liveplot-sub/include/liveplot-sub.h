74afb4f85dd76d91d443eb0dadec08acc27de314
Harald Held <harald.held@gmail.com>
2022-10-22 13:40:08 +0200

    Add some documentation to the subscriber's C interface.

--- lp-cpp/lib/liveplot-sub/include/liveplot-sub.h
+++ lp-cpp/lib/liveplot-sub/include/liveplot-sub.h
@@ -8,8 +8,21 @@ extern "C" {
 #endif
 
 typedef void *LivePlotSub;
+
+/**
+ * Callback o inform the subscriber about new values (x, y) for \a quantity. \a state is passed on as well.
+ */
 typedef void (*Callback)(double x, double y, const char *quantity, void *state);
 
+/**
+ * Subscribe to a quantity on a connection. \a state can be anything; nothing is done with it internally, it is just
+ * passed on to the callback \a cb.
+ * @param connection connection string to connect to (ipc://<path>/<to>/<pipe>, tpc://<host>:<port>)
+ * @param quantity topic to listen for and subscribe to
+ * @param state anything; it is passed to the callback \a cb on each call
+ * @param cb callback called to notify subscribers about new data
+ * @return
+ */
 API_LIVEPLOT_SUB_EXPORT LivePlotSub lpNewSubscription(const char *connection, const char *quantity, void *state,
                                                       Callback cb);
 API_LIVEPLOT_SUB_EXPORT void        lpDestroySubscription(LivePlotSub sub);
